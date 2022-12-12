e6cf7815c2c19b5b8398b2ac5e1f7ff56845d72d
DanieleAntonaci <danieleanto96@gmail.com>
2022-12-12 15:34:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script >
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header,
+
+  }
+}
+</script>
+
+<template>
+<font-awesome-icon icon="fa-solid fa-user" />
+  
+  <HelloWorld />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script >
+export default {
+  data() {
+    return {
+      na
+    }
+  }
+}
+</script>
+
+<template>
+ 
+</template>
+
+<style lang="scss" scoped>
+
+</style>
